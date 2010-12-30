949b4fa0544c04df2f150a412eecfad02211eec2
Kelly Harrington <kharring@andrew.cmu.edu>
2010-12-29 22:04:25 -0500

    starting off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KellyHope::Application.config.secret_token = '7614c7e550e8defd3b9f4697a11a1b7791bb14e3d11aa01ec56eb0eb84db1480b970f954f96d0452646ab4f392f245f7d9f6fc0996c95d20968759a32e8ece95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KellyHope::Application.config.session_store :cookie_store, :key => '_kelly_hope_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KellyHope::Application.config.session_store :active_record_store
